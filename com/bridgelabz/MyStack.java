99cb89546d8105a6b77a953b8082c4853830532d
unknown <papisettynagendra>
2022-08-22 18:21:01 +0530

    My first message

---
+++ com/bridgelabz/MyStack.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.LinkedList;
+public class MyStack {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Stack And Queue");
+    }
+
+}
